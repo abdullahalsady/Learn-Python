9220f0391f67a206dfbb057cb73b83ca69989916
Abdullah Al Sady <abdullahalsady09@gmail.com>
2023-05-16 09:48:00 +0600

    Second Day's Practice Done

---
+++ Practice/Day-02/datatypes.py
@@ -0,0 +1,48 @@
+# string
+string = "Abdullah Al Sady"
+print(type(string))
+print('--------------------')
+
+
+# number [integer, floating-point, complex]
+# integer number
+integer_number = 10
+print(type(integer_number))
+print('--------------------')
+
+# floating-point number
+float_point = 10.1
+print(type(float_point))
+print('--------------------')
+
+# complex number
+complex_number = 10j
+print(type(complex_number))
+print('--------------------')
+
+# sequence type data [list, tuple, range]
+# list
+list_datatype = ['one', 'two', 'three']
+print(type(list_datatype))
+print('--------------------')
+
+# tuple
+tuple_datatype = ("bangladesh", "india", 'pakistan')
+print(type(tuple_datatype))
+print('--------------------')
+
+# range
+range_datatype = range(1, 6)
+print(range_datatype)
+print(type(range_datatype))
+print('--------------------')
+
+# maping type
+dict_datatype = {"name": "abdullah", "age": 19}
+print(dict_datatype)
+print(type(dict_datatype))
+print('--------------------')
+
+'''
+There are many data types in Python. At this time those are not important.
+'''
