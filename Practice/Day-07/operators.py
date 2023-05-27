bbea1031c712ab88afb651328c504d8f5680b3da
Abdullah Al Sady <abdullahalsady09@gmail.com>
2023-05-27 16:34:18 +0600

    Seventh Day Practice Done.

---
+++ Practice/Day-07/operators.py
@@ -0,0 +1,124 @@
+# Arithmetic Operators
+# Addition
+first_number = 30
+second_number = 20
+print(first_number + second_number)
+
+# Subtraction
+first_number = 10
+second_number = 2
+print(first_number - second_number)
+
+# Division
+first_number = 10
+second_number = 5
+print(first_number / second_number)
+
+# Multiplication
+first_number = 3
+second_number = 3
+print(first_number * second_number)
+
+# Modulus
+first_number = 10
+second_number = 3
+print(first_number % second_number)
+
+# Exponentiation
+first_number = 2
+second_number = 3
+print(first_number ** second_number) # same as 2 * 2 * 2
+
+# Floor division
+first_number = 13
+second_number = 3
+print(first_number // second_number) # the floor division // rounds the result down to the nearest whole number
+
+
+# Assignment Operators
+# Equal
+first_number = 10
+
+# Plus Equal
+first_number = 10
+first_number += 1
+print(first_number)
+
+# Minus Equal
+first_number = 5
+first_number -= 1
+print(first_number)
+
+# Multiplication Equal
+first_number = 5
+first_number *= 2
+print(first_number)
+
+# Division Equal
+first_number = 10
+first_number /= 5
+print(first_number)
+
+# Modulus Equal
+first_number = 10
+first_number %= 3
+print(first_number)
+
+# Exponentiation Equal
+first_number = 10
+first_number **= 3
+print(first_number)
+
+
+# Comparison Operators
+# Equal
+first_number = 10
+second_number = 20
+print(first_number == second_number)
+
+# Not Equal 
+first_number = 10
+second_number = 5
+print(first_number != second_number)
+
+# Greater than
+first_number = 10
+second_number = 5
+print(first_number > second_number)
+
+# Less than
+first_number = 4
+second_number = 10
+print(first_number < second_number)
+
+# Greater than and Equal
+first_number = 10
+second_number = 10
+print(first_number >= second_number)
+
+# Less than and Equal
+first_number = 20
+second_number = 25
+print(first_number <= second_number)
+
+
+# Logical Operators
+# And
+first_number = 10
+second_number = 20
+print((first_number < second_number) and (first_number == second_number))
+
+# Or
+first_number = 10
+second_number = 10
+print((first_number < second_number) or (first_number == second_number))
+
+# Not 
+first_number = 10
+second_number = 5
+print(not((first_number < second_number) and (first_number == second_number)))
+
+
+# =====================================
+# Operator Precedence (Most important)
+# =====================================
